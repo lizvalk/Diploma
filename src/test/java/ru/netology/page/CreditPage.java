f2e6627e12fc37a6773eeee88ec89560613e8197
Lizvalk <lizvalk@mail.ru>
2023-12-12 22:01:09 +0700

    Added tests to the CreditPageTest

--- src/test/java/ru/netology/page/CreditPage.java
+++ src/test/java/ru/netology/page/CreditPage.java
@@ -2,6 +2,9 @@ package ru.netology.page;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
+import ru.netology.data.DataHelper;
+
+import java.time.Duration;
 
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
@@ -16,16 +19,42 @@ public class CreditPage {
     private final SelenideElement cvcCodeField = $("[placeholder='999']");
     private final SelenideElement buyButton = $$("span.button__text").get(2);
 
+    private final SelenideElement errorMessage = $("span.input__sub");
+    private final SelenideElement errorNotification = $(".notification_status_error .notification__content");
+    private final SelenideElement notificationStatusOk = $(".notification_status_ok .notification__content");
+
     public CreditPage() {
         heading.shouldBe(Condition.visible);
     }
-//    public void fillingCardData () {
-//        cardNumberField.setValue();
-//        monthField.setValue();
-//        yearField.setValue();
-//        cardholderField.setValue();
-//        cvcCodeField.setValue();
-//        buyButton.click();
-//    }
+    public void fillingCardData (DataHelper.CardInfo cardInfo) {
+        cardNumberField.setValue(cardInfo.getCardNumber());
+        monthField.setValue(cardInfo.getMonth());
+        yearField.setValue(cardInfo.getYear());
+        cardholderField.setValue(cardInfo.getCardholder());
+        cvcCodeField.setValue(cardInfo.getValidationCode());
+        buyButton.click();
+    }
+    public void shouldSuccessfulPayment() {
+        notificationStatusOk.shouldHave(Condition.text("Операция одобрена Банком"), Duration.ofSeconds(15));
+        notificationStatusOk.shouldBe(Condition.visible);
+    }
+    public void shouldDeclinedTransaction() {
+        errorNotification.shouldHave(Condition.text("Ошибка! Банк отказал в проведении операции."), Duration.ofSeconds(15));
+        errorNotification.shouldBe(Condition.visible);
+    }
+    public void wrongFormat() {
+        errorMessage.shouldBe(Condition.visible).shouldHave(Condition.text("Неверный формат"));
+    }
+    public void expiredNotification() {
+        errorMessage.shouldBe(Condition.visible).shouldHave(Condition.text("Истёк срок действия карты"));
+    }
+    public void cardExpirationDateIncorrect() {
+        errorMessage.shouldBe(Condition.visible).shouldHave(Condition.text("Неверно указан срок действия карты"));
+    }
+    public void requiredField() {
+        errorMessage.shouldBe(Condition.visible).shouldHave(Condition.text("Поле обязательно для заполнения"));
+    }
+
+
 
 }
--- src/test/java/ru/netology/test/CreditPageTest.java
+++ src/test/java/ru/netology/test/CreditPageTest.java
@@ -1,14 +1,193 @@
 package ru.netology.test;
 
-import org.junit.jupiter.api.BeforeEach;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.*;
+import ru.netology.data.DataHelper;
+import ru.netology.data.SQLHelper;
 import ru.netology.page.HomePage;
 
 import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CreditPageTest {
     HomePage homePage;
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
+
     @BeforeEach
     void setUp() {
         homePage = open("http://localhost:8080/", HomePage.class);
     }
+    @AfterEach
+    void tearDown() {
+        SQLHelper.cleanDB();
+    }
+    @Test
+    void shouldSuccessfulWhenDataValidForCardApproved() {
+        var cardInfo = DataHelper.getApprovedCardInfo();
+        var cardPage = homePage.goToCreditPage();
+        cardPage.fillingCardData(cardInfo);
+        cardPage.shouldSuccessfulPayment();
+        assertEquals("APPROVED", SQLHelper.getCreditRequestStatus());
+    }
+    @Test
+    void shouldRefusalPurchaseUsingCardWithDECLINEDStatus() { //все поля валидные карта 2
+        var cardInfo = DataHelper.getDeclinedCardInfo();
+        var cardPage = homePage.goToCreditPage();
+        cardPage.fillingCardData(cardInfo);
+        cardPage.shouldDeclinedTransaction();
+        assertEquals("DECLINED", SQLHelper.getCreditRequestStatus());
+    }
+    @Test
+    void shouldSuccessfulIfCardholderDataWithHyphen() { //все поля валидные. Поле Владелец через дефис
+        var cardInfo = DataHelper.getCardholderHyphenated();
+        var cardPage = homePage.goToCreditPage();
+        cardPage.fillingCardData(cardInfo);
+        cardPage.shouldSuccessfulPayment();
+        assertEquals("APPROVED", SQLHelper.getCreditRequestStatus());
+    }
+    @Test
+    void shouldErrorWhenCardNumberEmpty() { //поле Номер карты пустое - неверный формат
+        var cardInfo = DataHelper.getEmptyCardNumber();
+        var cardPage = homePage.goToCreditPage();
+        cardPage.fillingCardData(cardInfo);
+        cardPage.wrongFormat();
+    }
+    @Test
+    void shouldErrorWhenCardNumberWithSymbolsOrLetters() { //поле Номер карты заполненное символами/буквами - неверный формат
+        var cardInfo = DataHelper.getCardNumberGetFilledLettersOrSymbols();
+        var cardPage = homePage.goToCreditPage();
+        cardPage.fillingCardData(cardInfo);
+        cardPage.wrongFormat();
+    }
+    @Test
+    void shouldErrorWhenCardNumberWithLessThan16Digits() { //поле Номер карты заполненное 15ю символами - неверный формат
+        var cardInfo = DataHelper.getNumberCard15Symbols();
+        var cardPage = homePage.goToCreditPage();
+        cardPage.fillingCardData(cardInfo);
+        cardPage.wrongFormat();
+    }
+    @Test
+    void shouldErrorWhenMonthEmpty() { //поле месяц пустое - неверный формат
+        var cardInfo = DataHelper.getEmptyMonth();
+        var cardPage = homePage.goToCreditPage();
+        cardPage.fillingCardData(cardInfo);
+        cardPage.wrongFormat();
+    }
+    @Test
+    void shouldErrorWhenMonthWithSymbolsOrLetters() { //поле месяц заполненное символами/буквами - неверный формат
+        var cardInfo = DataHelper.getMonthGetFilledLettersOrSymbols();
+        var cardPage = homePage.goToCreditPage();
+        cardPage.fillingCardData(cardInfo);
+        cardPage.wrongFormat();
+    }
+    @Test
+    void shouldErrorWhenMonthWithOneDigit() { //поле месяц заполненное одной цифрой - неверный формат
+        var cardInfo = DataHelper.getMonthFilledOneDigit();
+        var cardPage = homePage.goToCreditPage();
+        cardPage.fillingCardData(cardInfo);
+        cardPage.wrongFormat();
+    }
+    @Test
+    void shouldPaymentCardInvalidMonthOver12() { //поле месяц заполненное цифрой 13
+        var cardInfo = DataHelper.getCardMonthOver12();
+        var cardPage = homePage.goToCreditPage();
+        cardPage.fillingCardData(cardInfo);
+        cardPage.cardExpirationDateIncorrect();
+    }
+    @Test
+    void shouldPaymentCardInvalidWhen00Month () { //поле месяц заполненное цифрой 00
+        var cardInfo = DataHelper.getMonthFilled00();
+        var cardPage = homePage.goToCreditPage();
+        cardPage.fillingCardData(cardInfo);
+        cardPage.cardExpirationDateIncorrect();
+    }
+    @Test
+    void shouldPaymentCardInvalidWhenMonthPreviousToThisYear() { //поле месяц заполненное
+        var cardInfo = DataHelper.getCardMonthPreviousToThisYear();
+        var cardPage = homePage.goToCreditPage();
+        cardPage.fillingCardData(cardInfo);
+        cardPage.cardExpirationDateIncorrect();
+    }
+    @Test
+    void shouldErrorWhenYearEmpty() {//поле год пустое
+        var cardInfo = DataHelper.getEmptyYear();
+        var cardPage = homePage.goToCreditPage();
+        cardPage.fillingCardData(cardInfo);
+        cardPage.wrongFormat();
+    }
+    @Test
+    void shouldCardExpiredIfDataFromPreviousYear() { //в поле год данные пред года
+        var cardInfo = DataHelper.getExpiredCard();
+        var cardPage = homePage.goToCreditPage();
+        cardPage.fillingCardData(cardInfo);
+        cardPage.expiredNotification();
+    }
+    @Test
+    void shouldCardExpirationIncorrectIfData5YearsMoreThenCurrentOne() { //в поле год данные на 5 лет больше текущ
+        var cardInfo = DataHelper.getCurrentYearPlus5Years();
+        var cardPage = homePage.goToCreditPage();
+        cardPage.fillingCardData(cardInfo);
+        cardPage.cardExpirationDateIncorrect();
+    }
+    @Test
+    void shouldErrorIfInYearOnlyOneDigit() { //в поле год 1 цифра
+        var cardInfo = DataHelper.getOneDigitInYear();
+        var cardPage = homePage.goToCreditPage();
+        cardPage.fillingCardData(cardInfo);
+        cardPage.wrongFormat();
+    }
+    @Test
+    void shouldErrorIfCardholderEmpty() { //пустое поле владелец
+        var cardInfo = DataHelper.getEmptyCardholder();
+        var cardPage = homePage.goToCreditPage();
+        cardPage.fillingCardData(cardInfo);
+        cardPage.requiredField();
+    }
+    @Test
+    void shouldErrorIfInCardholderFieldWithNumbers() { //в поле Владелец введены цифры
+        var cardInfo = DataHelper.getNumbersInCardholder();
+        var cardPage = homePage.goToCreditPage();
+        cardPage.fillingCardData(cardInfo);
+        cardPage.wrongFormat();
+    }
+    @Test
+    void shouldErrorIfCyrillicInCardholderField() { //Данные о владельце карты указаны неверно: имя и фамилия на кириллице
+        var cardInfo = DataHelper.getDataInCyrillic();
+        var cardPage = homePage.goToCreditPage();
+        cardPage.fillingCardData(cardInfo);
+        cardPage.wrongFormat();
+    }
+    @Test
+    void shouldErrorIfInCardholderWithSymbols() { //Данные о владельце карты указаны неверно: введены символы
+        var cardInfo = DataHelper.getDataInSymbols();
+        var cardPage = homePage.goToCreditPage();
+        cardPage.fillingCardData(cardInfo);
+        cardPage.wrongFormat();
+    }
+    @Test
+    void shouldErrorWhenCVCEmpty() {//пустое поле CVC
+        var cardInfo = DataHelper.getEmptyCVC();
+        var cardPage = homePage.goToCreditPage();
+        cardPage.fillingCardData(cardInfo);
+        cardPage.wrongFormat();
+    }
+    @Test
+    void shouldPaymentCardInvalidCvc2Symbols() { //Невалидный код CVC: ввод менее 3 цифр
+        var cardInfo = DataHelper.get2SymbolsInCVC();
+        var cardPage = homePage.goToCreditPage();
+        cardPage.fillingCardData(cardInfo);
+        cardPage.wrongFormat();
+    }
+
+
 }
